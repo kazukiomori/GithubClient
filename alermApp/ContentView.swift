375088615057ecbac6f4416b4421e654fadff811
kazukiomori <kazuki1110170501@gmail.com>
2022-06-22 00:03:37 +0900

    Initial Commit

---
+++ alermApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alermApp
+//
+//  Created by Kazuki Omori on 2022/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alermApp/alermAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  alermAppApp.swift
+//  alermApp
+//
+//  Created by Kazuki Omori on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct alermAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
